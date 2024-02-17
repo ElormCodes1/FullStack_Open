836b59475547b96d44857a534ad03d57502663b4
ElormCodes1 <marriondokosi@gmail.com>
2024-02-17 10:27:47 +0000

    1.2: course information, step2

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -1,21 +1,50 @@
+const course = 'Half Stack application development'
+const part1 = 'Fundamentals of React'
+const exercises1 = 10
+const part2 = 'Using props to pass data'
+const exercises2 = 7
+const part3 = 'State of a component'
+const exercises3 = 14
+
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
   )
 }
 
-const Content = (props) => {
+const Part1 = (props) => {
+  console.log(props)
+  return (
+    <p>
+      {props.part1} {props.exercises1}
+    </p>
+  )
+}
+
+const Part2 = (props) => {
+  console.log(props)
+  return (
+    <p>
+      {props.part2} {props.exercises2}
+    </p>
+  )
+}
+
+const Part3 = (props) => {
+  console.log(props)
+  return(
+    <p>
+      {props.part3} {props.exercises3}
+    </p>
+  )
+}
+
+const Content = () => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part1 part1={part1} exercises1={exercises1}/>
+      <Part2 part1={part2} exercises1={exercises2}/>
+      <Part3 part3={part3} exercises3={exercises3}/>
     </div>
   )
 }
@@ -27,22 +56,10 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
   return (
     <div>
       <Header course={course} />
-      <Content 
-        part1={part1} exercises1={exercises1} 
-        part2={part2} exercises2={exercises2} 
-        part3={part3} exercises3={exercises3} 
-      />
+      <Content />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
